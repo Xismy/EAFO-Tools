5cfb38c9eba3201fd0fbe57e270d7c7e533836d2
danimegar <danimegar@gmail.com>
2017-04-01 21:20:57 +0200

    Mejora de las adaptaciones
    Mejora el calculo de la adaptación

--- matlab/Adaptacion_Stub_cortocircuito_y_linea.m
+++ matlab/Adaptacion_Stub_cortocircuito_y_linea.m
@@ -20,8 +20,10 @@ for l1=0:0.0001:0.5
         Zx=(Zolinea*(ZL+1j*Zolinea*tan(2*pi*l2)))/(Zolinea+1j*ZL*tan(2*pi*l2)); % Impedancia al inicio de la linea
         Zy=1j*Zostub*tan(2*pi*l1); %Impedancia del stub (el stub está en cortocircuito)
         Zz=(Zx*Zy)/(Zx+Zy); %Paralelo 
-            if abs(real(Zz))>= abs(real(Zin)*0.999) && abs(real(Zz))<=abs(real(Zin)*1.001)
-                if abs(imag(Zz))>= abs(imag(Zin)*0.999) && abs(imag(Zz))<=abs(imag(Zin)*1.001)
+        
+        if imag(Zin)>=0
+            if real(Zz)>= real(Zin)*0.999 && real(Zz)<=real(Zin)*1.001
+                if imag(Zz)>= imag(Zin)*0.999 && imag(Zz)<=imag(Zin)*1.001
                     L1=l1 %%% Longitud del Stub
                     w1=fcalcular_w(Zostub,er,L1)  %% Ancho del Stub
                     L2=l2 %%%% Longitud de la linea
@@ -29,6 +31,17 @@ for l1=0:0.0001:0.5
                     Zz % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
                 end
             end
+        else
+            if real(Zz)>= real(Zin)*0.999 && real(Zz)<=real(Zin)*1.001
+                if imag(Zz)<= imag(Zin)*0.999 && imag(Zz)>=imag(Zin)*1.001
+                    L1=l1 %%% Longitud del Stub
+                    w1=fcalcular_w(Zostub,er,L1)  %% Ancho del Stub
+                    L2=l2 %%%% Longitud de la linea
+                    w2=fcalcular_w(Zolinea,er,L2)  %%% Ancho de la linea
+                    Zz % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
+                end
+            end
+        end
     end
 end
 
