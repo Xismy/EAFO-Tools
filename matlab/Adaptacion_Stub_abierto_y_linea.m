20f117f11313e80ff7bfcea18d012599faceb116
danimegar <danimegar@gmail.com>
2017-04-12 16:03:04 +0200

    Añadido display de L y W en cm

--- matlab/Adaptacion_Stub_abierto_y_linea.m
+++ matlab/Adaptacion_Stub_abierto_y_linea.m
@@ -5,10 +5,13 @@ clc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Parametros
+c=3*10^8; %Valor fijo:velocidad de la luz
+
 Zolinea = 50; %Impedancia caracteristica linea
 Zostub = 50; %Impendancia caracteristica del stub
 ZS=15-1j*25; % Impedancia de fuente
 ZL=35+1j*20; % Impedancia de carga
+f=4*10^9;  % Frecuencia
 er=2.2;
 
 Zin=conj(ZS); %Impedancia donde tenemos que llegar. Debe ser conjugado de ZS
@@ -24,9 +27,12 @@ for l1=0:0.0001:0.5
         if imag(Zin)>=0
             if real(Zz)>= real(Zin)*0.999 && real(Zz)<=real(Zin)*1.001
                 if imag(Zz)>= imag(Zin)*0.999 && imag(Zz)<=imag(Zin)*1.001
-                    Lstub=l1 %%% Longitud del Stub
+                    Lstub=l1*c*100/f %%Longitud del stub en cm
+                    %Lstub=l1 %%% Longitud del Stub en Lambda
                     Wstub=fcalcular_w(Zostub,er,Lstub)  %% Ancho del Stub
-                    Llinea=l2 %%%% Longitud de la linea
+                    
+                    Llinea=l2*c*100/f %%%% Longitud de la linea en cm
+                    %Llinea=l2   %%% Longitud de la linea en Lambda
                     Wlinea=fcalcular_w(Zolinea,er,Llinea)  %%% Ancho de la linea
                     Zz % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
                 end
@@ -34,9 +40,12 @@ for l1=0:0.0001:0.5
         else
             if real(Zz)>= real(Zin)*0.999 && real(Zz)<=real(Zin)*1.001
                 if imag(Zz)<= imag(Zin)*0.999 && imag(Zz)>=imag(Zin)*1.001
-                    Lstub=l1 %%% Longitud del Stub
+                    Lstub=l1*c*100/f %%Longitud del stub en cm
+                    %Lstub=l1 %%% Longitud del Stub en Lambda
                     Wstub=fcalcular_w(Zostub,er,Lstub)  %% Ancho del Stub
-                    Llinea=l2 %%%% Longitud de la linea
+                    
+                    Llinea=l2*c*100/f %%%% Longitud de la linea en cm
+                    %Llinea=l2   %%% Longitud de la linea en Lambda
                     Wlinea=fcalcular_w(Zolinea,er,Llinea)  %%% Ancho de la linea
                     Zz % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
                 end
