0dc56a09929fca2c44fd8f20f67efda6f137585c
jgarciagimenez <josemanuelgarciagimenez@gmail.com>
2017-04-29 22:05:35 +0200

    Matlab mejor adaptación
    Mejora en el algoritmo de búsqueda de la solución óptima para encontrar
    la mejor directamente.

--- matlab/Adaptacion_Stub_abierto_y_linea.m
+++ matlab/Adaptacion_Stub_abierto_y_linea.m
@@ -6,20 +6,20 @@ clc
 
 %Parametros
 c=3*10^8; %velocidad de la luz
-er=2.5; %permitividad electrica relativa
+er=3.5; %permitividad electrica relativa
 e0=8.8542*10^-12; %permitividad electrica absoluta
 u0=4*pi*10^-7; %permeabilidad magnética;
 v=1/sqrt(u0*e0*er); %velocidad
 
 Zolinea = 50; %Impedancia caracteristica linea
 Zostub = 50; %Impendancia caracteristica del stub
-ZS=15-1j*25; % Impedancia de fuente
-ZL=35+1j*20; % Impedancia de carga
+ZS=50; % Impedancia de fuente
+ZL=25+1j*50; % Impedancia de carga
 f=4*10^9;  % Frecuencia
 
 Zin=conj(ZS); %Impedancia donde tenemos que llegar. Debe ser conjugado de ZS
 
-lstub_vector =[];
+Lstub_vector =[];
 Llinea_vector = [];
 Zz_vector = [];
 contador = 1;
@@ -31,46 +31,47 @@ for l1=0:0.0001:0.5
         Zz=(Zx*Zy)/(Zx+Zy); %Paralelo 
         
         if imag(Zin)>=0
-            if real(Zz)>= real(Zin)*0.999 && real(Zz)<=real(Zin)*1.001
-                if imag(Zz)>= imag(Zin)*0.999 && imag(Zz)<=imag(Zin)*1.001
 
-                    Lstub=l1*v*100/f %Longitud del stub en cm
-                    Lstub_vector(contador) = Lstub; 
-                    %Lstub=l1 % Longitud del Stub en Lambda
-                    
-                    Llinea=l2*v*100/f % Longitud de la linea en cm
-                    %Llinea=l2  % Longitud de la linea en Lambda
-                    Llinea_vector(contador) = Llinea; 
-                    Zz % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
-                    
-                    Zz_vector(contador) = Zz;
-                    contador = contador +1;
-                end
-            end
+            Lstub=l1*v*100/f ;%Longitud del stub en cm
+            Lstub_vector(contador) = Lstub; 
+            %Lstub=l1 % Longitud del Stub en Lambda
+
+            Llinea=l2*v*100/f; % Longitud de la linea en cm
+            %Llinea=l2  % Longitud de la linea en Lambda
+            Llinea_vector(contador) = Llinea; 
+            Zz; % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
+
+            Zz_vector(contador) = Zz;
+            contador = contador +1;
+
         else
-            if real(Zz)>= real(Zin)*0.999 && real(Zz)<=real(Zin)*1.001
-                if imag(Zz)<= imag(Zin)*0.999 && imag(Zz)>=imag(Zin)*1.001
 
-                    Lstub_vector(contador) = Lstub; 
-                    Lstub=l1*v*100/f % Longitud del stub en cm
-                    %Lstub=l1 % Longitud del Stub en Lambda
-                    
-                    Llinea=l2*v*100/f % Longitud de la linea en cm
-                    %Llinea=l2  % Longitud de la linea en Lambda
-                    Llinea_vector(contador) = Llinea; 
+            Lstub=l1*v*100/f ;% Longitud del stub en cm
+            %Lstub=l1 % Longitud del Stub en Lambda
+            Lstub_vector(contador) = Lstub; 
+            Llinea=l2*v*100/f; % Longitud de la linea en cm
+            %Llinea=l2  % Longitud de la linea en Lambda
+            Llinea_vector(contador) = Llinea; 
 
-                    Zz % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
-                       Zz_vector(contador) = Zz;
-                       contador = contador+1;
-                end
-            end
+            Zz; % Saldran muchas soluciones, mirad cual se parece más a conj(ZS)
+            Zz_vector(contador) = Zz;
+            contador = contador+1;
+
         end
     end
 end
 
 [A B]=min(Zz_vector-Zin);
+
+disp('La impedancia que se obtiene es:')
+
 Zz_vector(B)
+
+disp('La longitud del stub es:')
+
 Lstub_vector(B)
+
+disp('La longitud de la línea es:')
 Llinea_vector(B)
 
 
