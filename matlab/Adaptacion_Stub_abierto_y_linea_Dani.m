9ba5c26111ac7025e403764565ce0db01f17a013
danimegar <danimegar@gmail.com>
2017-05-04 17:03:28 +0200

    Mejorado script Dani Adaptacion Linea y Stub

--- matlab/Adaptacion_Stub_abierto_y_linea_Dani.m
+++ matlab/Adaptacion_Stub_abierto_y_linea_Dani.m
@@ -9,7 +9,7 @@ c=3*10^8; %Valor fijo
 er=3.5; %permitividad electrica relativa
 e0=8.8542*10^-12; %permitividad electrica absoluta
 u0=4*pi*10^-7; %permeabilidad magnética;
-v=1/sqrt(u0*e0*er); %velocidad
+h=0.11; %Espesor del dielectrico en cm
 
 Zolinea = 50; %Impedancia caracteristica linea
 Zostub = 50; %Impendancia caracteristica del stub
@@ -18,6 +18,26 @@ ZL=25-1j*24; % Impedancia de carga
 f=4*10^9; %Frecuencia
 
 Zin=conj(ZS); %Impedancia donde tenemos que llegar. Debe ser conjugado de ZS
+
+%Calculo de er_eff
+%En el caso de que la Zo del stub y de linea sean diferentes, habría que
+%hacer 2 veces esta función y guardar la ereff_linea y ereff_stub.
+A=(Zolinea/60)*sqrt((er+1)/2)+((er-1)/(er+1))*(0.23+(0.11/er));
+B=(377*pi)/(2*Zolinea*sqrt(er));
+
+wh1=(8*exp(A))/((exp(2*A))-2);
+wh2=(2/pi)*(B-1-log(2*B-1)+((er-1)/(2*er))*(log(B-1)+0.39-(0.61/er)));
+
+if (wh1 < 2 )
+    wh=wh1;
+else
+    wh=wh2;
+end
+
+ereff=((er+1)/2)+((er-1)/(2*sqrt(1+(12/wh))));
+
+%Calculo de la velocidad de propagación
+v=1/sqrt(u0*e0*ereff); %velocidad
 
 for l1=0:0.0001:0.5
     for l2=0:0.0001:0.5
